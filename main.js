561022f8de9a333c64a6bfc32228ee4163214eba
Unknown <ou18ssa96ma@gmail.com>
2019-04-15 01:12:00 +0100

    update index and main

--- main.js
+++ main.js
@@ -1,70 +1,59 @@
 var routes = [{
-    path: '/',
-    component: httpVueLoader('./pages/Home.vue')
-},{
-  path: '/keys',
+  path: '/',
+  component: httpVueLoader('./pages/Home.vue')
+}, {
+  path: '/steam',
   component: httpVueLoader('./pages/Keys.vue')
-},{
+}, {
   path: '/settings',
   component: httpVueLoader('./pages/Settings.vue')
-},{
+}, {
   path: '/about',
   component: httpVueLoader('./pages/About.vue')
-},
-];
+}, ];
 const router = new VueRouter({
-routes
+  routes
 });
 new Vue({
-    el: '#app',
-    router,
-    data: ({
-        show: true,
-      
-        items: [
-          {
-            icon: 'home',
-            title: 'Home',
-            link: '/'
-          },
-          {
-            title: 'My Keys',
-            link: '/keys',
-            subitems :[{
-              icon: 'list',
-              title: 'Steam',
-              link: '/keyssteam'
-            },{
-              icon: 'list',
-              title: 'Origin',
-              link: '/keysorigin'
-            },{
-              icon: 'list',
-              title: 'Uplay',
-              link: '/keysuplay'
-            },
-            {
-              icon: 'list',
-              title: 'Other',
-              link: '/keysother'
-            }]
-          },
-          
-          { divider: true },
-          { header: 'OTHER' },
-          {
-            icon: 'settings',
-            title: 'Settings',
-            link: '/settings'
-          },
-          
-          {
-            icon: 'help',
-            title: 'About',
-            link: 'about'
-          }
-        ],
-          mini: false,
-          keys : true,
-           }),
+  el: '#app',
+  router,
+  data: ({
+    show: true,
+    items: [{
+        title: 'Home',
+        icon: 'mdi-home',
+        link: '/'
+      }, {
+        title: 'Steam',
+        icon: 'mdi-steam',
+        link: '/steam'
+      }, {
+        title: 'Uplay',
+        icon: 'mdi-ubisoft',
+        link: '/uplay'
+      },
+      {
+        title: 'Origin',
+        icon: 'mdi-origin',
+        link: '/origin'
+      },
+      {
+        title: 'Other',
+        icon: 'mdi-alert-circle',
+        link: '/other'
+      },
+      {
+        title: 'Settings',
+        icon: 'mdi-settings',
+        link: '/settings'
+      },
+      {
+        title: 'About',
+        icon: 'mdi-help-circle',
+        link: '/about'
+      }
+    ],
+    mini: false,
+    keys: true,
+  }),
 })
