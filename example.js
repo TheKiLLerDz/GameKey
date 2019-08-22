494640a33e6d7adcde58ab2fe12b3652c8ac352b
Mehdi Malah <clash_azer@hotmail.com>
2019-08-22 10:51:24 +0100

    edit example.js

--- example.js
+++ example.js
@@ -2,15 +2,38 @@
 function impport() {
 const lineByLine = require('./readlines.js');
 const liner = new lineByLine('./text.txt');
-
+var linestr;
+var index;
+var word;
 let line;
 let lineNumber = 0;
 
 while (line = liner.next()) {
-    console.log('Line ' + lineNumber + ': ' + line.toString('ascii'));
+   linestr = line.toString('ascii');
+   word = linestr.split(' ')
+   if (validateKey(word[0])) {
+       console.log('key : ' + word[0])
+       word = word.filter((el) => {
+        return el !== word[0];})
+       console.log('tagname : ' + word)
+   }else {
+       index = word["length"] - 1
+       console.log('key : ' + word[index])
+    word = word.filter((el) => {
+        return el !== word[index];})
+    console.log('tagname : ' + word)
+
+   }
+   console.log(word)
     lineNumber++;
 }
 
 console.log('end of line reached');
 }
 
+function validateKey(key) {
+    // Pour vérifier j'ai utilisé pattern email
+
+    // var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    // return re.test(String(email).toLowerCase());
+}
