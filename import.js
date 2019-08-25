8370903d61aecb9bda8654598ea5dae2386eaa1a
Mehdi Malah <clash_azer@hotmail.com>
2019-08-25 11:24:25 +0100

    edit method baseorxhr

--- import.js
+++ import.js
@@ -2,93 +2,101 @@
 var keys = []
 var key = [];
 var tagname = [];
-function impport() {
-const lineByLine = require('./readlines.js');
-const liner = new lineByLine('./text.txt');
-var linestr;
-var index;
-var word;
-let line;
-let lineNumber = 0;
 
-while (line = liner.next()) {
-   linestr = line.toString('ascii');
-   word = linestr.split(' ')
-   if (validateKey(word[0])) {
-     key.push(word[0])
-       word = word.filter((el) => {
-        return el !== word[0] & el !== '';})
-      var  tag = word.join(' ');
-       tagname.push(tag)
-   }else {
-       index = word["length"] - 1
-       key.push(word[index])
-    word = word.filter((el) => {
-        return el !== word[index] & el !== '';})
-        var  tag = word.join(' ');
-        tagname.push(tag)   }
+function impport() {
+  const lineByLine = require('./readlines.js');
+  const liner = new lineByLine('./text.txt');
+  var linestr;
+  var index;
+  var word;
+  let line;
+  let lineNumber = 0;
+
+  while (line = liner.next()) {
+    linestr = line.toString('ascii');
+    word = linestr.split(' ')
+    if (validateKey(word[0])) {
+      key.push(word[0])
+      word = word.filter((el) => {
+        return el !== word[0] & el !== '';
+      })
+      var tag = word.join(' ');
+      tagname.push(tag)
+    } else {
+      index = word["length"] - 1
+      key.push(word[index])
+      word = word.filter((el) => {
+        return el !== word[index] & el !== '';
+      })
+      var tag = word.join(' ');
+      tagname.push(tag)
+    }
     lineNumber++;
-}
-base()
+  }
+  baseorxhr()
 }
 
 function validateKey(key) {
-    // Pour vérifier j'ai utilisé pattern email
+  // Pour vérifier j'ai utilisé pattern email
 
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(key).toLowerCase());
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(key).toLowerCase());
 }
-function sendData(i,callback) {
-     
-    var xhr = new XMLHttpRequest();
 
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState === 4) {
-        // console.log(xhr.response); 
-        filtrer(xhr.responseText,i)
-      }
+function sendData(i, callback) {
+
+  var xhr = new XMLHttpRequest();
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      // console.log(xhr.response); 
+      filtrer(xhr.responseText, i)
     }
-  
-    xhr.open('GET', 'https://store.steampowered.com/search/suggest?term=p&f=games&cc=DZ&l=french&excluded_content_descriptors%5B%5D=3&excluded_content_descriptors%5B%5D=4&v=6766867', true);
-    xhr.send('');
-    
   }
-  
-  function filtrer(tag,i) {
-    var el = document.createElement('div');
-    el.innerHTML = tag
-   var l = el.getElementsByTagName('a').item(0)
-var appid = l.attributes[1].value
-  addtokeys(appid,i)
+
+  xhr.open('GET', 'https://store.steampowered.com/search/suggest?term=p&f=games&cc=DZ&l=french&excluded_content_descriptors%5B%5D=3&excluded_content_descriptors%5B%5D=4&v=6766867', true);
+  xhr.send('');
+
+}
+
+function filtrer(tag, i) {
+  var el = document.createElement('div');
+  el.innerHTML = tag
+  var l = el.getElementsByTagName('a').item(0)
+  var appid = l.attributes[1].value
+  addtokeys(appid, i)
 
 
-  }
-function Data() {
+}
+
+function baseorxhr() {
   var i = 0;
-while (i < tagname.length) {
-  sendData(i)
-  i++
+  while (i < tagname.length) {
+    var j = 0;
+    var test = true
+    while (j < store.state.steam.length & test) {
+      if (tagname[i] == store.state.steam[j].name) {
+        addtokeys(store.state.steam[j].appid, i)
+        test = false;
+      }
+      j++
     }
+
+    i++
+
+  }
 }
-  function base(){
-var i = 0;
-while (i < tagname.length) {
- tagname = testappname(1,i)
-  i++
-    }
-    
-    setTimeout(function () {
-
-    Data(),4000})
-  }
-  
-
-  function addtokeys(appid,i) {
-    keys.push({'key' : key[i],'appid': appid})
-
- 
-  }
 
 
-  
-  // var promise =  
+function addtokeys(appid, i) {
+  keys.push({
+    'key': key[i],
+    'appid': appid
+  })
+
+
+}
+
+
+
+// var promise =  
