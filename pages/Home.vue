480690de7396ac3803c5d65fd2a0b81ba017f55d
TheKiLLerDz <ramaziagaba@gmail.com>
2019-09-17 11:15:55 +0100

    update home txt

--- pages/Home.vue
+++ pages/Home.vue
@@ -2,7 +2,7 @@
     <v-layout row wrap>
         <v-flex v-for="item in items" :key="item.title" xs12 sm12 md6 lg4>
             <v-card elevation="2" class="mb-4 px-3 py-0" style='border-radius:10px'>
-                <v-card :color="item.color"
+                <v-card :color="item.color" :to="{path:item.link}"
                     style="top:-24px;margin: 0px 16px 0px;padding: 16px;border-radius: 8px;position: absolute;"
                     elevation="3" :class="item.class">
                     <v-icon color='white' size="50">{{item.icon}}</v-icon>
@@ -11,8 +11,8 @@
                     <h2 class="text-xs-right">{{item.title}}</h2>
                     <v-divider :color="item.color"></v-divider>
                 </v-card-text>
-                <div class="text-xs-center">Your Game Count : {{item.games}}</div>
-                <div class="text-xs-center">With : {{item.keys}} Keys </div>
+                <div class="text-xs-center">Library : {{item.games}}</div>
+                <div class="text-xs-center">Keys : {{item.keys}} </div>
                 <v-card-actions>
                     <v-divider :color="item.color"></v-divider>
                     <v-btn flat white :to="{path:item.link}" :color="item.color" round outline>
