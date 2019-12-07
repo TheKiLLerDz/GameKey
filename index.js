69b72cbf7d622d43469ec5373197ca4f8ec89a55
TheKiLLerDz <ramaziagaba@gmail.com>
2019-12-07 15:20:25 +0100

    add vue & vuetify v2 offline & changed js names

--- index.js
+++ index.js
@@ -1,97 +1,237 @@
+CreateDB()
 const {
-    app,
-    BrowserWindow,
-    ipcMain,
-    dialog,
-    screen
+  ipcRenderer
 } = require('electron')
-var mainwin, Loginwin;
+var routes = [{
+  path: '/',
+  component: httpVueLoader('./pages/Home.vue')
+}, {
+  path: '/keys',
+  component: httpVueLoader('./pages/Keys.vue')
+}, {
+  path: '/keys/:searchvalue',
+  component: httpVueLoader('./pages/Keys.vue'),
+  props: true
+}, {
+  path: '/steam',
+  component: httpVueLoader('./pages/Keys.vue')
+}, {
+  path: '/origin',
+  component: httpVueLoader('./pages/Keys.vue')
+}, {
+  path: '/uplay',
+  component: httpVueLoader('./pages/Keys.vue')
+}, {
+  path: '/other',
+  component: httpVueLoader('./pages/Keys.vue')
+}, {
+  path: '/settings',
+  component: httpVueLoader('./pages/Settings.vue')
+}, {
+  path: '/about',
+  component: httpVueLoader('./pages/About.vue')
+}, ];
+const router = new VueRouter({
+  routes
+});
 
-app.setPath('userData', "C:/OneDrive/GameKey")
-
-function createAppWindow() {
-    mainwin = new BrowserWindow({
-        width: 800,
-        height: 500,
-        transparent: true,
-        frame: false,
-        webPreferences: {
-            nodeIntegration: true,
-            //devTools: false
-        }
-    })
-
-    mainwin.loadURL('file://' + __dirname + '/index.html')
-}
-
-function createLoginWindow() {
-    const {
-        width,
-        height
-    } = screen.getPrimaryDisplay().workAreaSize;
-    Loginwin = new BrowserWindow({
-        width: width * 0.5,
-        height: height * 0.7,
-        transparent: true,
-        frame: false,
-        webPreferences: {
-            nodeIntegration: true
-        }
-    })
-
-    Loginwin.loadURL('file://' + __dirname + '/Login.html')
-}
-
-app.on('ready', createLoginWindow)
-
-ipcMain.on('minimize-app', () => {
-    if (mainwin != null)
-        mainwin.minimize();
-    else Loginwin.minimize();
+const store = new Vuex.Store({
+  state: {
+    userdata: {
+      username: localStorage.username,
+      avatar: localStorage.avatar,
+      password: '*'.repeat(localStorage.pwlength)
+    },
+    finished: false,
+    steam: [],
+    steamkey: [],
+    uplay: [],
+    uplaykey: [],
+    infodialog: false,
+    moreinfo:{
+      Developer: 'Undefined',
+      Publisher: 'Undefined',
+      Genre: '',
+      Price: '0'
+    },
+    origin: [],
+    updatedb: {},
+    originkey: [],
+    others: [],
+    otherskey: [],
+    allkeys: [],
+    temp: {},
+    tempimport: undefined,
+    import: false,
+    waitingdialog: false,
+    importedapps: [],
+  },
 })
-
-ipcMain.on('maximize-app', () => {
-    mainwin.maximize();
-})
-ipcMain.on('access-app', () => {
-    createAppWindow();
-    Loginwin.hide();
-})
-
-ipcMain.on('unmaximize-app', () => {
-    mainwin.setSize(800, 500);
-    mainwin.center();
-})
-
-ipcMain.on('close-app', () => {
-    if (mainwin != null) mainwin.close();
-    Loginwin.close();
-})
-
-ipcMain.on('Path-request', (event, Platform) => {
-    dialog.showOpenDialog(mainwin, {
-        properties: ['openFile'],
-        filters: [{
-                name: 'Supported Files',
-                extensions: ['txt', 'xlsx']
-            },
-            {
-                name: 'Text Files',
-                extensions: ['txt']
-            },
-            {
-                name: 'Excel Files',
-                extensions: ['xlsx']
-            },
-            {
-                name: 'All Files',
-                extensions: ['*']
+v = new Vue({
+  el: '#app',
+  store,
+  router,
+  data: ({
+    App: {
+      version: '1.0',
+      year: '2019'
+    },
+    Launch: false,
+    loading: false,
+    theme: '',
+    themes: [{
+        name: 'Dark Theme',
+        color: 'dark',
+        class: 'theme--dark',
+      },
+      {
+        name: 'Light Theme',
+        color: 'success',
+        class: 'theme--light',
+      }, {
+        name: 'Blue Theme',
+        color: 'blue',
+        class: 'theme--blue',
+      }
+    ],
+    selected: null,
+    search: null,
+    settingstab: false,
+    show: true,
+    items: [{
+        title: 'Home',
+        icon: 'mdi-home',
+        link: '/'
+      }, {
+        title: 'All Keys',
+        color: '#0e82cf',
+        icon: 'mdi-key',
+        link: '/keys'
+      }, {
+        title: 'Steam',
+        color: '#1d2f54',
+        icon: 'mdi-steam',
+        link: '/steam'
+      }, {
+        title: 'Uplay',
+        color: '#0e82cf',
+        icon: 'mdi-ubisoft',
+        link: '/uplay'
+      },
+      {
+        title: 'Origin',
+        color: 'orange',
+        icon: 'mdi-origin',
+        link: '/origin'
+      },
+      {
+        title: 'Other',
+        color: 'success',
+        icon: 'mdi-alert-circle',
+        link: '/other'
+      },
+      {
+        title: 'Settings',
+        icon: 'mdi-settings',
+        link: '/settings'
+      },
+      {
+        title: 'About',
+        icon: 'mdi-help-circle',
+        link: '/about'
+      }
+    ],
+    keys: true,
+    isDark: false,
+    mini: false,
+    windowWidth: 0,
+    Maximized: null,
+    updatedb: []
+  }),
+  methods: {
+    Minimize() {
+      ipcRenderer.send('minimize-app');
+    },
+    Maximize() {
+      this.Maximized = !this.Maximized;
+      // var targLink = document.getElementById("demo");
+      // var clickEvent = document.createEvent('MouseEvents');
+      // clickEvent.initEvent('dblclick', true, true);
+      // targLink.dispatchEvent(clickEvent);
+    },
+    Close() {
+      ipcRenderer.send('close-app');
+    },
+    customFilter(item, queryText) {
+      const textOne = item.name.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+      const textTwo = String(item.appid).toLowerCase();
+      const searchText = queryText.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+      return textOne.indexOf(searchText) > -1 ||
+        textTwo.indexOf(searchText) > -1
+    },
+    ReadNotif(title, index) {
+      this.updatedb.splice(index, 1);
+    },
+    Update(item) {
+      this.updatedb[item].type == 'ND' ? updateDB(JSON.parse(localStorage.getItem("version"))) : this.updatedb[item].type == 'NA' ? console.log('very soon') : console.log('Link')
+    },
+    getNotif() {
+      setTimeout(() => {
+        if (store.state.updatedb.notifications != undefined)
+          store.state.updatedb.notifications.forEach(el => {
+            if (!el.value || el.value != 'true') {
+              el.value = 'false';
+              this.updatedb.push(el);
             }
-        ]
-    }).then(result => {
-        if (!result.canceled) event.reply('Path-reply', result.filePaths, Platform)
-    }).catch(err => {
-        console.log(err)
+          })
+      }, 2000);
+    },
+  },
+  computed: {
+    userdata() {
+      return {
+        username: store.state.userdata.username,
+        avatar: store.state.userdata.avatar,
+        password: store.state.userdata.password
+      }
+    },
+    games() {
+      return store.state.steamkey.concat(store.state.uplaykey.concat(store.state.originkey
+        .concat(store.state.otherskey)));
+    }
+  },
+  beforeCreate() {
+    opendb()
+  },
+  mounted() {
+    if (!localStorage.Patterns) localStorage.Patterns = true;
+    this.Maximized = screen.width == window.innerWidth && screen.height == window.innerHeight
+    if (localStorage.theme) this.theme = localStorage.theme;
+    if (localStorage.theme) this.isDark = (localStorage.Dark == 'true');
+    this.windowWidth = window.innerWidth;
+    this.$nextTick(() => {
+      window.addEventListener('resize', () => {
+        this.windowWidth = window.innerWidth
+      });
     })
+    this.getNotif();
+  },
+  watch: {
+    // Maximized(newValue) {
+    //   console.log('newValue ' + newValue)
+    //   newValue ? ipcRenderer.send('maximize-app') : ipcRenderer.send('unmaximize-app')
+    // },
+    windowWidth(newWidth, oldWidth) {
+      //console.log(newWidth)
+      if (newWidth >= 0.6 * screen.width) this.mini = false
+      else this.mini = true
+    },
+    theme(mytheme) {
+      localStorage.theme = mytheme;
 
+    },
+    isDark(value) {
+      localStorage.Dark = value;
+    }
+  }
 })
