0fa2bfe0530dc418b5e2350e68be36cc77d8979a
TheKiLLerDz <ramaziagaba@gmail.com>
2019-12-27 22:54:46 +0100

    add about template

--- templates/About.vue
+++ templates/About.vue
@@ -1,16 +1,24 @@
 <template>
     <v-layout row wrap>
-        <v-flex lg4>
-            <h1>About Template</h1>
-        </v-flex>
-    </v-layout>
+        <v-flex xs12 sm12 md12 lg12>
+            <v-card class="elevation-5 px-2"
+                style="border-radius: 8px; height: calc(100% - 70px);padding: 0px 0px 75px 0px;">
+                <v-card class='yellowcard white--text unselectable'
+                    style="top:-30px; padding: 15px;border-radius: 8px;">
+                    <h1>
+                        <v-icon dense color='white' x-large>
+                            mdi-help-circle
+                        </v-icon>
+                        About GameKey
+                    </h1>
+                </v-card>
+                <v-card-text>
+                </v-card-text>
 </template>
-
 <script>
     module.exports = {
         data() {
-            return {
-                
+            return {}
         }
-    }}
+    }
 </script>
